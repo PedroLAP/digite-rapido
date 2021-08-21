1b17a981add42e9467041fb6e838cef524cf8d5e
PedroLA <pedropaulola2007@gmail.com>
2021-08-21 18:49:43 -0300

    Nova mudança na posição da chamada para 'radom'

--- js/main.js
+++ js/main.js
@@ -96,12 +96,12 @@ function finalizaJogo() {
 }
 
 function reiniciaJogo() {
-    radom(lista.length, 0);
     campo.attr('disabled', false);
     campo.val('');
     $('#contador-palavras').text(0);
     $('#contador-caracteres').text(0);
     $('#tempo-digitacao').text(tempoInicial);
+    radom(lista.length, 0);
     inicializaCronometro();
     campo.removeClass('campo-desativado');
     campo.removeClass('borda-vermelha');
